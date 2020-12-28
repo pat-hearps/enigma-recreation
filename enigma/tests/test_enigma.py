093619b0591cfb7b6265e605871f3be2bfd68b09
Unknown <pat.hearps@gmail.com>
2020-12-28 15:42:58 +1100

    test for full_scramble

--- enigma/tests/test_enigma.py
+++ enigma/tests/test_enigma.py
@@ -26,14 +26,14 @@ def test_set_current_position(eg) -> None:
     assert eg.pos_rgt_rotor == entry.index(menu_link[2])
 
 
-dat_no_ring = [
+once_thru_dat_no_ring = [
     ("AAA", "GVURPWXIQJANZLYKMEOFBSTCHD", "KUXZRTAYHJPNQLSEIDVWCBFGOM"),
     # ("AAB", "DPGVRHFEYUOAQMWLNCZJKBITSX", "LVRAHGCFWTUPNQKBMEYXJDOZIS"), # depends if we define end as RRo or Entry
     ("AKA", "FLMGYCTIQWVBJPNUXRAKDESOZH", "SLFUVADZHMTBCOXNIRWGPKJQEY"),
     ("NAA", "LQTMKJSHVWCBOGDPRFZENXUAIY", "UMPBQGCWIJVZSXLKOTRNHYFEAD")
 ]
 
-@pytest.mark.parametrize("menu_link, exp_forward, exp_reverse", dat_no_ring)
+@pytest.mark.parametrize("menu_link, exp_forward, exp_reverse", once_thru_dat_no_ring)
 def test_once_thru_scramble_no_ring_settings(menu_link, exp_forward, exp_reverse, eg) -> None:
     eg.set_current_position(menu_link=menu_link)
     # forward direction
@@ -48,4 +48,21 @@ def test_once_thru_scramble_no_ring_settings(menu_link, exp_forward, exp_reverse
         ans = eg.once_thru_scramble(in_char, direction='back', first_rotor=eg.left_rotor, pos1=eg.pos_left_rotor,
                                     second_rotor=eg.middle_rotor, pos2=eg.pos_mid_rotor,
                                     third_rotor=eg.right_rotor, pos3=eg.pos_rgt_rotor)
+        assert ans == expected
+
+
+full_data_no_ring = [
+    ("AAA", "NFXUHBJERGOPWAKLSIQVDTMCZY"),
+    ("AAB", "FWPOVAYMLRNIHKDCUJXZQEBSGT"),
+    ("AKA", "WDXBSPYNVKJRQHTFMLEOZIACGU"),
+    ("NAA", "UWZNJYRPKEISTDQHOGLMAXBVFC")
+]
+
+
+@pytest.mark.parametrize("menu_link, exp_out", full_data_no_ring)
+def test_full_scramble_no_ring_settings(menu_link, exp_out, eg):
+    eg.set_current_position(menu_link=menu_link)
+
+    for in_char, expected in zip(entry, exp_out):
+        ans = eg.full_scramble(in_char)
         assert ans == expected
