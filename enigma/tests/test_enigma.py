8d37fef98afa9f9ccd1c71ea7c987b8b333fba11
Unknown <pat.hearps@gmail.com>
2020-12-28 15:04:29 +1100

    parametized tests for stepped rotors, no rings

--- enigma/tests/test_enigma.py
+++ enigma/tests/test_enigma.py
@@ -26,19 +26,26 @@ def test_set_current_position(eg) -> None:
     assert eg.pos_rgt_rotor == entry.index(menu_link[2])
 
 
-def test_once_thru_scramble(eg) -> None:
+dat_no_ring = [
+    ("AAA", "GVURPWXIQJANZLYKMEOFBSTCHD", "KUXZRTAYHJPNQLSEIDVWCBFGOM"),
+    # ("AAB", "DPGVRHFEYUOAQMWLNCZJKBITSX", "LVRAHGCFWTUPNQKBMEYXJDOZIS"), # depends if we define end as RRo or Entry
+    ("AKA", "FLMGYCTIQWVBJPNUXRAKDESOZH", "SLFUVADZHMTBCOXNIRWGPKJQEY"),
+    ("NAA", "LQTMKJSHVWCBOGDPRFZENXUAIY", "UMPBQGCWIJVZSXLKOTRNHYFEAD")
+]
+
+@pytest.mark.parametrize("menu_link, exp_forward, exp_reverse", dat_no_ring)
+def test_once_thru_scramble_no_ring_settings(menu_link, exp_forward, exp_reverse, eg) -> None:
+    eg.set_current_position(menu_link=menu_link)
     # forward direction
-    for in_char, expected in zip(entry, "GVURPWXIQJANZLYKMEOFBSTCHD"):
+    for in_char, expected in zip(entry, exp_forward):
         ans = eg.once_thru_scramble(in_char, direction='forward', first_rotor=eg.right_rotor, pos1=eg.pos_rgt_rotor,
                                     second_rotor=eg.middle_rotor, pos2=eg.pos_mid_rotor,
                                     third_rotor=eg.left_rotor, pos3=eg.pos_left_rotor)
-
         assert ans == expected
 
     # reverse direction
-    for in_char, expected in zip(entry, "KUXZRTAYHJPNQLSEIDVWCBFGOM"):
+    for in_char, expected in zip(entry, exp_reverse):
         ans = eg.once_thru_scramble(in_char, direction='back', first_rotor=eg.left_rotor, pos1=eg.pos_left_rotor,
                                     second_rotor=eg.middle_rotor, pos2=eg.pos_mid_rotor,
                                     third_rotor=eg.right_rotor, pos3=eg.pos_rgt_rotor)
-
         assert ans == expected
