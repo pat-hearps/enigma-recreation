594fbf8439477125b53363930688fdadb9880288
Unknown <pat.hearps@gmail.com>
2022-03-05 16:02:00 +1100

    FEAT: add proper logging

--- enigma/utils.py
+++ enigma/utils.py
@@ -1,8 +1,24 @@
+import logging
 import os
+import sys
 
+LOG_FORMAT = "%(levelname)-5s | %(asctime)s | %(module)s:%(lineno)s-3s | %(message)s"
+DT_FORMAT = "%H:%M:%S"
 
 def vprint(message: str, msg_level: int, v_level: int = None):
     if v_level is None:
         v_level = int(os.getenv("verbosity", default="0"))
     if msg_level <= v_level:
         print(message)
+
+
+def get_logger(name: str, level: str = "DEBUG") -> logging.Logger:
+
+    formatter = logging.Formatter(fmt=LOG_FORMAT, datefmt=DT_FORMAT)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(stream_handler)
+    return logger
