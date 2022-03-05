e8b51ff9f886b6c5645761f06b76ffea4e0f3c0e
Unknown <pat.hearps@gmail.com>
2022-03-05 21:27:45 +1100

    FEAT: parametrize input cribs to menu test

--- enigma/tests/test_menu.py
+++ enigma/tests/test_menu.py
@@ -2,20 +2,32 @@ import pytest
 
 from enigma.menu import MenuMaker
 
-crib = "TOTHEPRESIDENTOFTHEUNITEDSTATES"
-encoded_crib = "CQNZPVLILPEUIKTEDCGLOVWVGTUFLNZ"
+CRIB_SETS = {
+    "welchman": {
+        "crib_guess": "TOTHEPRESIDENTOFTHEUNITEDSTATES",
+        "crib_cypher": "CQNZPVLILPEUIKTEDCGLOVWVGTUFLNZ"
+    },
+    "dermot": {
+        "crib_guess": "",
+        "crib_cypher": ""
+    }
+}
+
+gw_all_loops = {frozenset(('E', 'I', 'P')), frozenset(('I', 'P', 'V')), frozenset(('N', 'O', 'T'))}
 
 
 loop_data = [
-    (0, 14, {frozenset(('E', 'I', 'P'))}),
-    (3, 23, {frozenset(('E', 'I', 'P')), frozenset(('I', 'P', 'V')), frozenset(('N', 'O', 'T'))}),
-    (14, 27, set())
+    ("welchman", 0, 14, {frozenset(('E', 'I', 'P'))}),
+    ("welchman", 3, 23, gw_all_loops),
+    ("welchman", 14, 27, set())
 ]
 
 
-@pytest.mark.parametrize("start, end, expected", loop_data)
-def test_menumaker_loops(start: int, end: int, expected: set) -> None:
-    menu_mkr = MenuMaker(crib=crib[start:end], encoded_crib=encoded_crib[start:end])
+@pytest.mark.parametrize("crib_set_name, start, end, expected", loop_data)
+def test_menumaker_loops(crib_set_name: str, start: int, end: int, expected: set) -> None:
+    crib_guess = CRIB_SETS[crib_set_name]["crib_guess"]
+    crib_cypher = CRIB_SETS[crib_set_name]["crib_cypher"]
+    menu_mkr = MenuMaker(crib=crib_guess[start:end], encoded_crib=crib_cypher[start:end])
     menu_mkr.process_stuff()
     found_loops = set(frozenset(loop) for loop in menu_mkr.found_loops)
     assert found_loops == expected
