1ad8ee4e230df434a9c05c506b7632d44f176270
Unknown <pat.hearps@gmail.com>
2022-03-05 21:09:36 +1100

    TEST: parametrize gordon welchman crib

--- enigma/tests/test_menu.py
+++ enigma/tests/test_menu.py
@@ -6,8 +6,18 @@ crib = "TOTHEPRESIDENTOFTHEUNITEDSTATES"
 encoded_crib = "CQNZPVLILPEUIKTEDCGLOVWVGTUFLNZ"
 
 
-def test_menumaker() -> None:
-    menu_mkr = MenuMaker(crib=crib, encoded_crib=encoded_crib)
+loop_data = [
+    (0, 14, {frozenset(('E', 'I', 'P'))}),
+    (3, 23, {frozenset(('E', 'I', 'P')), frozenset(('I', 'P', 'V')), frozenset(('N', 'O', 'T'))}),
+    (14, 27, set())
+]
+
+
+@pytest.mark.parametrize("start, end, expected", loop_data)
+def test_menumaker_loops(start: int, end: int, expected: set) -> None:
+    menu_mkr = MenuMaker(crib=crib[start:end], encoded_crib=encoded_crib[start:end])
     menu_mkr.process_stuff()
-    assert 0
+    found_loops = set(frozenset(loop) for loop in menu_mkr.found_loops)
+    assert found_loops == expected
+    # assert 0
 
