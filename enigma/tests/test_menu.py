18466d69f47df3474a6797906f80ecffa7792814
Unknown <pat.hearps@gmail.com>
2022-03-05 16:09:24 +1100

    TEST: start of menu testing

--- enigma/tests/test_menu.py
+++ enigma/tests/test_menu.py
@@ -1,2 +1,13 @@
+import pytest
+
+from enigma.menu import MenuMaker
+
 crib = "TOTHEPRESIDENTOFTHEUNITEDSTATES"
 encoded_crib = "CQNZPVLILPEUIKTEDCGLOVWVGTUFLNZ"
+
+
+def test_menumaker() -> None:
+    menu_mkr = MenuMaker(crib=crib, encoded_crib=encoded_crib)
+    menu_mkr.process_stuff()
+    assert 0
+
