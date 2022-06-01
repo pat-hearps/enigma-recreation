35a1f671137ed47a6f66253d9f38fa56283c83ee
Unknown <pat.hearps@gmail.com>
2022-06-01 21:45:17 +1000

    TEST: extend setup test

--- tests/test_bombe.py
+++ tests/test_bombe.py
@@ -6,3 +6,7 @@ def test_bombe_init() -> None:
     bombe = Bombe(menu=B1.menu, left_rotor=B1.left_rotor, middle_rotor=B1.middle_rotor,
                   right_rotor=B1.right_rotor, reflector=B1.reflector)
     assert bombe.test_char == 'A'
+    assert bombe.identity_scrambler.current_position == "ZZA"
+    bombe.light_character()
+    lit_characters = [char for char, status in bombe.register['status'].items() if status == 1]
+    assert lit_characters == ['A']
